569b715f06cdcad461dfb11af17f08f273f29b6c
Jan <jan.herlyn@tum.de>
2016-12-19 16:23:15 +0100

    we do not need sndfile anymore

--- cmake-build-debug/energy_daq_interface/CMakeFiles/energy_daq_inteface.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libenergy_daq_inteface.a"
-)
--- cmake-build-debug/event_detector/CMakeFiles/event_detector_doc.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/event_detector_doc"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/event_detector_doc.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()

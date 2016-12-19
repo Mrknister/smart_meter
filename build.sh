b98d14739ed503889e1561f93c3c975269caee49
Jan <jan.herlyn@tum.de>
2016-12-19 16:07:39 +0100

    removed dlib

--- build.sh
+++ build.sh
@@ -11,19 +11,8 @@ if [ ! -d $build_dir ]; then
     mkdir $build_dir
 fi
 
-dlib_build_dir=external/dlib/build
-if [ ! -d $dlib_build_dir ]; then 
-    mkdir $dlib_build_dir
-fi
-
-cd $dlib_build_dir
-cmake ..
-cmake --build . --config Release
 
 
-
-
-cd $project_root_dir
 libnabo_build_dir=external/libnabo/build
 if [ ! -d $libnabo_build_dir ]; then
     mkdir $libnabo_build_dir
